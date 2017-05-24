8ce1c2ef8da17dfeeeb88dcfaa2eed56bc2dddc2
Taosky <t@firefoxcn.net>
2017-05-24 11:36:36 +0800

    compass

---
+++ app/src/main/java/science/zxc/walkin/ChangeImageView.java
@@ -0,0 +1,18 @@
+package science.zxc.walkin;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * AUTH: Taosky
+ * TIME: 2017/5/23 0023:下午 11:27.
+ * MAIL: t@firefoxcn.net
+ * DESC:
+ */
+
+
--- app/src/main/java/science/zxc/walkin/activity/MainActivity.java
+++ app/src/main/java/science/zxc/walkin/activity/MainActivity.java
@@ -1,13 +1,20 @@
 package science.zxc.walkin.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -36,8 +43,12 @@ public class MainActivity extends AppCompatActivity {
     private Button btnStop;//停止按钮
     private Button btnSave;//保存按钮
     private ImageView imgMap;//地图
+    private ImageView imgArrow;//指针
     private Canvas canvas;//画布
+    private Canvas arrowCanvas;//箭头的画布
     private Bitmap baseBitmap;//基础位图用于绘制
+    private Bitmap upBitmap;//用于绘制箭头
+    private ArrowView arrowView ;
     private boolean isStopped = true;// 用以判断是否已停止
     private boolean isPainted = false; //判断是否选择起点
     private float startX;//画笔起点
@@ -74,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //初始化
         imgMap = (ImageView) findViewById(R.id.img_map);
+        imgArrow = (ImageView) findViewById(R.id.img_arrow);
         btnStart = (Button) findViewById(R.id.start_button);
         btnStop = (Button) findViewById(R.id.stop_button);
         btnSave = (Button) findViewById(R.id.save_button);
@@ -81,12 +93,13 @@ public class MainActivity extends AppCompatActivity {
         txtDirection = (TextView) findViewById(R.id.direction);
         txtSteps = (TextView) findViewById(R.id.steps);
         initUI();//UI初始化
+        arrowView=new ArrowView(this);
+        arrowView.setBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.arrow));
         myWalk = new MyWalk();
-        //监听触屏事件
-        imgMap.setOnTouchListener(onTouch);
+        imgMap.setOnTouchListener(onTouch);//监听触屏事件
     }
 
-    //按钮点击事件
+    /*按钮点击事件*/
     public void onClick(View view) {
         switch (view.getId()) {
             //开始按钮
@@ -114,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //imageView触摸事件
+    /*imageView触摸事件*/
     private View.OnTouchListener onTouch = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -124,12 +137,16 @@ public class MainActivity extends AppCompatActivity {
                         imgMap.getHeight(), Bitmap.Config.ARGB_8888);
                 canvas = new Canvas(baseBitmap);
 
+                upBitmap = Bitmap.createBitmap(imgArrow.getWidth(),
+                        imgArrow.getHeight(), Bitmap.Config.ARGB_8888);
+                arrowCanvas= new Canvas(upBitmap);
+
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN && isStopped) {
                     //用户按下动作
                     startX = motionEvent.getX();
                     startY = motionEvent.getY();
                     paintPoint(startX, startY, BEGIN);//绘制起点
-                    Toast.makeText(MainActivity.this, "起点已选", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"起点已选",Toast.LENGTH_SHORT).show();
                     isPainted = true;
                     initUI();//选择起点后重置UI的信息
                     btnSave.setVisibility(View.INVISIBLE);//选择起点后保存按钮不可见
@@ -140,10 +157,11 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    //开始行走
+    /*开始行走*/
     private void start() {
         btnStart.setVisibility(View.INVISIBLE);//开始按钮不可见
         btnStop.setVisibility(View.VISIBLE);
+        imgArrow.setVisibility(View.VISIBLE);//显示箭头
         preDistance = 0;
         myWalk.start();
         isStopped = false;
@@ -151,6 +169,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 while (!isStopped) {
+                    Log.d("MainActivity", String.valueOf(myWalk.getDirection()));
                     updateUI(myWalk.getDistance(),myWalk.getDirection());//更新界面
                     try {
                         Thread.sleep(200);//每200ms获取一次
@@ -162,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    //停止行走
+    /*停止行走*/
     private void stop() {
         myWalk.stop();
         btnStop.setVisibility(View.INVISIBLE);//停止按钮不可见
@@ -172,11 +191,14 @@ public class MainActivity extends AppCompatActivity {
         paintPoint(startX, startY, END);
     }
 
-    //更新信息
+    /*更新信息*/
     private void updateUI(final float distance, final float direction) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                arrowView.setValues(startX,startY,direction);
+                arrowView.draw(arrowCanvas);
+                imgArrow.setImageBitmap(upBitmap);
                 txtSteps.setText(String.format(Locale.CHINA, "步数:%d", (int) (distance / 0.67)));
                 txtDistance.setText(String.format(Locale.CHINA, "距离:%.2f米", distance));//设置距离
                 txtDirection.setText(String.format(Locale.CHINA, "方向:%s",
@@ -186,19 +208,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //重置UI信息
+    /*重置UI信息*/
     private void initUI(){
         txtSteps.setText("步数:0");
         txtDistance.setText("距离:0米");//设置距离
         txtDirection.setText("方向:暂无");//设置方向
 
     }
-    //绘制起点、终点
+    /*绘制起点、终点*/
     private void paintPoint(float X, float Y, int status) {
         Paint paint = new Paint();
         if (status == BEGIN) {
             paint.setColor(Color.rgb(122, 195, 99));
         } else if (status == END) {
+            imgArrow.setVisibility(View.INVISIBLE);//隐藏指针
             paint.setColor(Color.RED);
         }
         canvas.drawCircle(X, Y, 13, paint);
@@ -206,30 +229,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //绘制路径(连线)
+    /*绘制路径(连线)*/
     private void paintLine(float distance, float direction) {
-        //没有移动直接返回
-        if (distance == 0) {
-            return;
-        }
-        //通过相对距离计算停止点的坐标
+       // imgArrow.layout((int) (startX-60), (int) (startY-60), imgArrow.getRight(),imgArrow.getBottom());
+       // Log.d("arrow",String.format("img:left:%s,top:%s,right:%s,bottom:%s",imgArrow.getX(),imgArrow.getY(),imgArrow.getRight(),imgArrow.getBottom()));
+        //通过相对距离
         float reDistance = distance - preDistance;
-        reDistance *= 15;// 相对距离转换成坐标距离
-        float stopX = (float) (startX + reDistance * Math.cos((direction - 90) * Math.PI / 180));
-        float stopY = (float) (startY + reDistance * Math.sin((direction - 90) * Math.PI / 180));
-        //绘制
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(3);
-        canvas.drawLine(startX, startY, stopX, stopY, paint);
-        imgMap.setImageBitmap(baseBitmap);//更新图片
-        //更新坐标和距离
-        startX = stopX;
-        startY = stopY;
-        preDistance = distance;
+        //没有移动直接返回
+        if (reDistance != 0) {
+           // Log.d("MainActivity", String.valueOf(reDistance));
+            //计算停止点的坐标
+            reDistance *= 15;// 相对距离转换成坐标距离
+            float x = (float) (direction * Math.PI / 180);
+            float stopX = (float) (startX + reDistance * Math.sin(x));
+            float stopY = (float) (startY -reDistance * Math.cos(x));
+            //Log.d("MainActivity","stop:"+String.valueOf(stopX)+","+String.valueOf(stopY));
+            //绘制
+            Paint paint = new Paint();
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(3);
+            canvas.drawLine(startX, startY, stopX, stopY, paint);
+            imgMap.setImageBitmap(baseBitmap);//更新图片
+            //更新坐标和距离
+            startX = stopX;
+            startY = stopY;
+            preDistance = distance;
+        }
     }
 
-    //保存行走记录
+    /*保存行走记录*/
     private void saveRecord() {
         Date currentTime = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
@@ -272,6 +300,55 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         if (isPainted) {
             stop();
+        }
+    }
+
+
+   private class ArrowView extends View{
+        private Bitmap arrowBitmap;
+        private float mX = 0;
+        private float mY = 0;
+        private float mDegree = 0;
+        private Paint paint;
+
+        public void setBitmap(Bitmap bitmap){
+            arrowBitmap = bitmap;
+        }
+
+        public void setValues(float x, float y, float degree){
+            mX = x;
+            mY = y;
+            mDegree =degree;
+        }
+
+
+        public ArrowView(Context context) {
+            super(context);
+           // setZOrderOnTop(true);
+          //  getHolder().setFormat(PixelFormat.TRANSLUCENT);
+            paint = new Paint();
+        }
+
+        public ArrowView(Context context, @Nullable AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public ArrowView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+        }
+
+        @Override
+        public void draw(Canvas canvas) {
+            canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//清空
+            // 定义矩阵对象
+            Matrix matrix = new Matrix();
+            matrix.setScale(0.3f,0.3f);
+            int offsetX = arrowBitmap.getWidth() *3/ 20;
+            int offsetY = arrowBitmap.getHeight() *3/ 20;
+            matrix.postTranslate(-offsetX, -offsetY);
+            matrix.postRotate(mDegree);
+            matrix.postTranslate(mX , mY );
+            canvas.drawBitmap(arrowBitmap, matrix, paint);
         }
     }
 }
